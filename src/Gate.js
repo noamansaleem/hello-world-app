2e7d69b15e9edd013602b6f223e84505eda62bc1
noamansaleem <noamantech@gmail.com>
2021-01-04 20:59:34 +0500

    Update Gate.js

--- src/Gate.js
+++ src/Gate.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 
 const Gate = ({ isOpen }) => {
-    return <p className="isOpen"><strong>{isOpen == true ? "open!" : "closed!"}</strong>
+    return <p className="isOpen"><strong>{isOpen === true ? "open!" : "closed!"}</strong>
         <br />
     This is Gate component.
     </p>
